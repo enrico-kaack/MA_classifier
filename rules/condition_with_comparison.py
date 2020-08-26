fcead995ae267a1be3ff3d6f0a50c57400607d10
enrico-kaack <e.kaack@live.de>
2020-08-26 17:37:12 +0200

    Fixed: condition rule checker now working with unary and bool operators

--- rules/condition_with_comparison.py
+++ rules/condition_with_comparison.py
@@ -12,6 +12,20 @@ class ConditionComparison():
             #for all if
             if isinstance(node, ast.If):
                 testNode = node.test
-                if not isinstance(testNode, ast.Call):
+                
+                if self._check_if_direct_comparison(testNode):
                     problems.append({"type": "CONDITION_COMPARISON", "line_number":  testNode.lineno, "col_offset": testNode.col_offset})
         return problems
+
+    def _check_if_direct_comparison(self, node):
+        if isinstance(node, ast.BoolOp):
+            violated = False
+            for n in node.values:
+                print("nodes", n)
+                if self._check_if_direct_comparison(n):
+                    violated = True
+            return violated
+        if isinstance(node, ast.UnaryOp):
+            return self._check_if_direct_comparison(node.operand)
+        if not  isinstance(node, ast.Call):
+            return True
