1e9fa4d6b427683c3f57ac64e10fbeef00c572e5
enrico-kaack <e.kaack@live.de>
2020-08-30 16:48:23 +0200

    Added: Finished validation code

--- run_validation.py
+++ run_validation.py
@@ -3,14 +3,14 @@ from tasks.gradient_boosting_classifier import TaskTrainGradientBoostingClassifi
 from tasks.svm import TaskTrainSVM
 from tasks.lstm import TaskTrainLstm
 from tasks.random_forest import TaskTrainRandomForest
-from tasks.manipulate_code import TaskEvalEnsemble
+from tasks.manipulate_code import TaskEvalEnsemble, TaskEvalKeras
 import d6tflow
 
-d6tflow.settings.log_level = 'WARNING' # 'DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'
+d6tflow.settings.log_level = 'DEBUG' # 'DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'
 
-model = TaskTrainRandomForest(max_vocab_size=100000, input_src_path="second_large_dataset", problem_type=ProblemType.RETURN_NONE, oversampling_enabled=True, ratio_after_oversampling=0.5).outputLoad()
+model = TaskTrainLstm(max_vocab_size=100000, input_src_path="second_large_dataset", problem_type=ProblemType.RETURN_NONE, oversampling_enabled=False, undersampling_enabled=True, ratio_after_undersampling=0.5, embedding_vecor_length=32, epochs=3, batch_size=64, encode_type=False, num_lstm_cells=10, dropout_emb_lstm=0.2, dropout_lstm_dense=0.2).outputLoad()
 
 
 
-t = TaskEvalEnsemble(model=model, input_src_path="second_large_dataset", problem_type=ProblemType.RETURN_NONE, vocab_input_directory="second_large_dataset", max_vocab_size=100000)
+t = TaskEvalKeras(model=model, input_src_path="validation", problem_type=ProblemType.RETURN_NONE, vocab_input_directory="second_large_dataset", max_vocab_size=100000, encode_type=False)
 d6tflow.run(t)
--- tasks/manipulate_code.py
+++ tasks/manipulate_code.py
@@ -31,7 +31,6 @@ class TaskCodeManipulator(d6tflow.tasks.TaskPickle):
                 code_string = d["src"]
                 lines = code_string.splitlines(keepends=False)
                 for i in problem_line_numbers:
-                    print("PROBLEM PARSING: ",d["file_path"],  i, problem_line_numbers, len(lines))
                     line = lines[i-1] #-1 since line numbers are indexed from 1, the array from 0
                     new_line = re.sub("return None", callback, line)
                     lines[i-1] = new_line
@@ -45,32 +44,49 @@ class TaskCodeManipulator(d6tflow.tasks.TaskPickle):
         
         self.save(data)
 
+
 from models.random_forst import process_general_data
-from utils.data_dumper import dump_json
-from utils.plotter import confusion_matrix, evaluate_model, plot_confusion_matrix
-from sklearn.metrics import confusion_matrix
 from tasks.preprocessing import TaskVocabCreator
 
 @d6tflow.inherits(TaskCodeManipulator)
-class TaskEvalEnsemble(d6tflow.tasks.TaskPickle):
-    model  = luigi.Parameter()
+class TaskPrepareXYValidation(d6tflow.tasks.TaskPickle):
     window_size = luigi.IntParameter(default=20)
     step_size = luigi.IntParameter(default=3)
     encode_type = luigi.BoolParameter(default=True)
     vocab_input_directory = luigi.Parameter(default="second_large_dataset")
     max_vocab_size = luigi.IntParameter(default=100000)
 
-
     def requires(self):
         return {"data": self.clone(TaskCodeManipulator), "vocab": TaskVocabCreator(max_vocab_size=self.max_vocab_size, input_src_path=self.vocab_input_directory)}
 
     def run(self):
+        print(f"###Running {type(self).__name__}")
+
+
         data = self.input()["data"].load()
         vocab = self.input()["vocab"].load()
 
         # prepare XY
         x,y = process_general_data(data, vocab, window_size=self.window_size, step_size=self.step_size, problem_type=self.problem_type.value, encode_type=self.encode_type)
         print(f"Length dataset: {len(x)}")
+
+        self.save((x,y))
+
+from utils.data_dumper import dump_json
+from utils.plotter import confusion_matrix, evaluate_model, plot_confusion_matrix
+from sklearn.metrics import confusion_matrix
+
+@d6tflow.inherits(TaskPrepareXYValidation)
+class TaskEvalEnsemble(d6tflow.tasks.TaskPickle):
+    model  = luigi.Parameter()
+
+    def requires(self):
+        return self.clone(TaskPrepareXYValidation)
+
+    def run(self):
+        print(f"###Running {type(self).__name__}")
+
+        x,y = self.input().load()
 
         #predict
         rf_predictions = self.model.predict(x)
@@ -90,3 +106,36 @@ class TaskEvalEnsemble(d6tflow.tasks.TaskPickle):
         parameter_dict = {k:v for (k,v) in self.__dict__["param_kwargs"].items() if k != "model"}
         dump_json(self.task_id, parameter_dict, results)
 
+from utils.data_dumper import dump_json
+from utils.plotter import confusion_matrix, evaluate_model, plot_confusion_matrix
+from sklearn.metrics import confusion_matrix
+
+@d6tflow.inherits(TaskPrepareXYValidation)
+class TaskEvalKeras(d6tflow.tasks.TaskPickle):
+    model  = luigi.Parameter()
+
+    def requires(self):
+        return self.clone(TaskPrepareXYValidation)
+
+    def run(self):
+        print(f"###Running {type(self).__name__}")
+
+        x,y = self.input().load()
+
+        #predict
+        rf_predictions = (self.model.predict(x) > 0.5).astype("int32")
+        rf_probs = self.model.predict(x)
+
+        #evaluate
+        metrics = evaluate_model(self.task_id, rf_predictions, rf_probs, y,  [], [], [], only_test=True)
+
+
+        # Confusion matrix
+        cm = confusion_matrix(y, rf_predictions)
+        cm_values = plot_confusion_matrix(self.task_id, cm, classes = ['0', '1'],
+                            title = 'Confusion Matrix', normalize=True)
+
+        #Write to file
+        results = {**metrics, **cm_values}
+        parameter_dict = {k:v for (k,v) in self.__dict__["param_kwargs"].items() if k != "model"}
+        dump_json(self.task_id, parameter_dict, results)
