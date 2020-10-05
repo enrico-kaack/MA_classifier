2e699d77c2db66b8d05bf720499f6a837d463206
enrico-kaack <e.kaack@live.de>
2020-10-05 15:11:37 +0200

    FIXED: train/test distribution split before over/undersampling --> now after

--- tasks/preprocessing.py
+++ tasks/preprocessing.py
@@ -148,11 +148,6 @@ class TaskTrainTestSplit(d6tflow.tasks.TaskPickle):
         c2.update(y_train)
         print("Before", c1.most_common(), "After", c2.most_common())
 
-        #split train into train and train_dev
-        #calc the train_dev percentage of the train size
-        train_dev_percent = self.train_dev_split_percentage / (1 - self.test_split_percentage)
-        X_train, X_train_dev, y_train, y_train_dev  = train_test_split(X_train, y_train, test_size=train_dev_percent, random_state=1)
-        
         print(f"BEFORE OVER/UNDERSAMPLING: Length Train: {len(X_train)}, length train_dev {len(X_train_dev)}, length Test {len(X_test)}")
 
 
@@ -163,6 +158,13 @@ class TaskTrainTestSplit(d6tflow.tasks.TaskPickle):
         if self.undersampling_enabled:
             undersample = RandomUnderSampler(sampling_strategy=self.ratio_after_undersampling, random_state=1)
             X_train, y_train = undersample.fit_resample(X_train, y_train)
+
+
+        #split train into train and train_dev
+        #calc the train_dev percentage of the train size
+        train_dev_percent = self.train_dev_split_percentage / (1 - self.test_split_percentage)
+        X_train, X_train_dev, y_train, y_train_dev  = train_test_split(X_train, y_train, test_size=train_dev_percent, random_state=1)
+        
 
         print(f"AFTER OVER/UNDERSAMPLING: Length Train: {len(X_train)}, length train_dev {len(X_train_dev)}, length Test {len(X_test)}")
         self.save({
