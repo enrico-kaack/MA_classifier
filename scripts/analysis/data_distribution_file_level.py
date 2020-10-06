cad8ce8e891fa61a6d6b2399d1ce078d99f562e0
enrico-kaack <e.kaack@live.de>
2020-10-06 15:05:37 +0200

    Script: analyse data on file level

---
+++ scripts/analysis/data_distribution_file_level.py
@@ -0,0 +1,36 @@
+from tasks.preprocessing import TaskRuleProcessor, ProblemType, TaskSourceFileToDataStructure
+from input.input_data_analysis import analyse_parsed_data, analyse_problems_per_project, analyse_files_per_project, analyse_total_file_size
+import d6tflow
+d6tflow.settings.log_level = 'WARNING' # 'DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'
+
+print("final_dataset TRAIN")
+d6tflow.run(TaskRuleProcessor(input_src_path="final_dataset"))
+problems = TaskRuleProcessor(input_src_path="final_dataset").outputLoad()
+analyse_parsed_data(problems)
+print("\nTotal File Size")
+analyse_total_file_size(problems)
+print("\nProblems per project")
+analyse_problems_per_project(problems, 2)
+print("\nFiles per project")
+analyse_files_per_project(problems, 2)
+
+print("final_test TEST")
+d6tflow.run(TaskRuleProcessor(input_src_path="final_test"))
+problems = TaskRuleProcessor(input_src_path="final_test").outputLoad()
+analyse_parsed_data(problems)
+print("\nTotal File Size")
+analyse_total_file_size(problems)
+print("\nProblems per project")
+analyse_problems_per_project(problems, 3)
+print("\nFiles per project")
+analyse_files_per_project(problems, 2)
+
+print("final_valdiation HOLDOUT")
+problems = TaskRuleProcessor(input_src_path="final_validation").outputLoad()
+analyse_parsed_data(problems)
+print("\nTotal File Size")
+analyse_total_file_size(problems)
+print("\nProblems per project")
+analyse_problems_per_project(problems, 4)
+print("\nFiles per project")
+analyse_files_per_project(problems, 4)
