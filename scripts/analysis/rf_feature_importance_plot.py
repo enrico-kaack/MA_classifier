73278a29713e7d9208540bd5de25efdc89dc682f
enrico-kaack <e.kaack@live.de>
2020-10-09 19:00:57 +0200

    Added: permutation importance

--- scripts/analysis/rf_feature_importance_plot.py
+++ scripts/analysis/rf_feature_importance_plot.py
@@ -1,5 +1,7 @@
 from tasks.preprocessing import ProblemType
 from tasks.random_forest import TaskTrainRandomForest
+from tasks.holdout_test import TaskPrepareXYHoldout
+from sklearn.inspection import permutation_importance
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -8,15 +10,19 @@ def run():
         t = TaskTrainRandomForest(problem_type=problem_type, oversampling_enabled=True, ratio_after_oversampling=0.5, undersampling_enabled=False, encode_type=True)
         model = t.outputLoad()
 
-        importances = model.feature_importances_
-        indices = np.argsort(importances)
+        data_task = TaskPrepareXYHoldout(encode_type=True, problem_type=problem_type)
+        x,y = data_task.outputLoad()
 
+        result = permutation_importance(model, x, y, n_repeats=10,
+                                        random_state=1, n_jobs=4)
+        sorted_idx = result.importances_mean.argsort()
 
-        plt.title('Feature Importances')
-        plt.barh(range(len(indices)), importances[indices], color='#8f63f4', align='center')
-        plt.yticks(range(len(indices)), indices)
-        plt.xlabel('Relative Importance')
-        plt.savefig(f"Importance_{problem_type.value}.pdf")
+        fig, ax = plt.subplots()
+        ax.boxplot(result.importances[sorted_idx].T,
+                vert=False, labels=sorted_idx)
+        ax.set_title("Permutation Importances (holdout set)")
+        fig.tight_layout()
+        plt.savefig(f"Importance_permuted{problem_type.value}.pdf")
 
 
 
