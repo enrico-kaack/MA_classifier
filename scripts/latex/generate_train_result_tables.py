8b0507d68bec7a29b60228423b75eca913c69cff
enrico-kaack <e.kaack@live.de>
2020-10-12 20:45:15 +0200

    Fixed: generate train_results tables

--- scripts/latex/generate_train_result_tables.py
+++ scripts/latex/generate_train_result_tables.py
@@ -19,10 +19,10 @@ latex_jinja_env = jinja2.Environment(
 
 def process_data(results_train_test, results_holdout, model):
     #merge train/test and holdout by using the task_id with a replacement of train->Evaluate
-    # results_holdout["task_id"] = results_holdout["task_id"].replace(r"Train", "Evaluate", regex=True)
+    results_holdout["task_id"] = results_holdout["task_id"].replace(r"Train", "Evaluate", regex=True)
 
-    # results = results_train_test.merge(results_holdout, on="task_id", suffixes=("", "_Y"))
-    results = results_train_test
+    results = results_train_test.merge(results_holdout, on="task_id", suffixes=("", "_Y"))
+    #svm results = results_train_test
     results["ratio_after_oversampling"][results.oversampling_enabled == False] = "-"
     results["ratio_after_undersampling"][results.undersampling_enabled == False] = "-"
 
@@ -55,8 +55,8 @@ def process_data(results_train_test, results_holdout, model):
 
     merged = random_forest_RN.merge(random_forest_CC, on=merge_columns, suffixes=("_LEFT1", "_RIGHT1"), how="outer").merge(random_forest_CCS, on=merge_columns, suffixes=("_LEFT2", "_RIGHT2"), how="outer")
 
-#    merged = merged.sort_values(by="test_f1_RN", ascending=False)
-    merged = merged.sort_values(by="f1_RN", ascending=False)
+    merged = merged.sort_values(by="test_f1_RN", ascending=False)
+#svm    merged = merged.sort_values(by="f1_RN", ascending=False)
 
     return (model, merged)
 
@@ -89,7 +89,7 @@ def read_holdout_data(input_dir):
 def print_data(data, output_file, template_file):
         rendered = []
         for model, d in data:
-                with open(f"generate_train_templates/{template_file}.{model.replace(' ', '_')}", "r") as f:
+                with open(f"{template_file}.{model.replace(' ', '_')}", "r") as f:
                     t = latex_jinja_env.from_string(f.read())
                     rendered.append(t.render(data=d.T.to_dict().values(), model=model))
         with open(output_file, "w") as o:
@@ -103,16 +103,9 @@ if __name__ == "__main__":
     parser.add_argument('output', type=str, help='Output File path')
     args = parser.parse_args()
 
-    # results_train_test = read_train_test_data(args.test)
-    # results_holdout = read_holdout_data(args.holdout)
-    # data = []
-    # for model in ["random forest", "gradient boosting classifier", "lstm"]:# "svm"
-    #     data.append(process_data(results_train_test, results_holdout, model))
-    # print_data(data, args.output, args.template)
-
     results_train_test = read_train_test_data(args.test)
-    results_holdout = pd.DataFrame([])
+    results_holdout = read_holdout_data(args.holdout)
     data = []
-    for model in ["svm"]:
+    for model in ["random forest", "gradient boosting classifier", "lstm"]:# "svm"
         data.append(process_data(results_train_test, results_holdout, model))
     print_data(data, args.output, args.template)
