f94689d981a24563287ed04e4bc5f193ad49e6e1
enrico-kaack <e.kaack@live.de>
2020-10-12 10:48:11 +0200

    Fixes: num_lstm on manipulate

--- scripts/run_experiments/test_manipulated_code.py
+++ scripts/run_experiments/test_manipulated_code.py
@@ -58,8 +58,8 @@ def run_all_tasks(validation_source, workers):
             TaskTrainLstm(problem_type=problem_type, oversampling_enabled=False, undersampling_enabled=False, embedding_vecor_length=16, epochs=2, batch_size=256, encode_type=False, num_lstm_cells=10, dropout_emb_lstm=0.2, dropout_lstm_dense=0.2),
             TaskTrainLstm(problem_type=problem_type, oversampling_enabled=False, undersampling_enabled=False, embedding_vecor_length=16, epochs=3, batch_size=256, encode_type=False, num_lstm_cells=10, dropout_emb_lstm=0.2, dropout_lstm_dense=0.2),
 
-            TaskTrainLstm(problem_type=problem_type, oversampling_enabled=False, undersampling_enabled=True, ratio_after_undersampling=0.5, embedding_vecor_length=32, epochs=3, batch_size=256, encode_type=False, dropout_emb_lstm=0.2, dropout_lstm_dense=0.2),
-            TaskTrainLstm(problem_type=problem_type, oversampling_enabled=True, undersampling_enabled=False, ratio_after_oversampling=0.5, embedding_vecor_length=32, epochs=3, batch_size=256, encode_type=False, dropout_emb_lstm=0.2, dropout_lstm_dense=0.2)
+            TaskTrainLstm(problem_type=problem_type, oversampling_enabled=False, undersampling_enabled=True, ratio_after_undersampling=0.5, embedding_vecor_length=32, epochs=3, batch_size=256, encode_type=False,num_lstm_cells=10, dropout_emb_lstm=0.2, dropout_lstm_dense=0.2),
+            TaskTrainLstm(problem_type=problem_type, oversampling_enabled=True, undersampling_enabled=False, ratio_after_oversampling=0.5, embedding_vecor_length=32, epochs=3, batch_size=256, encode_type=False,num_lstm_cells=10, dropout_emb_lstm=0.2, dropout_lstm_dense=0.2)
         ]
         task_list_keras.extend(t)
 
