ed335ed410ea7ef212329979c504927b4234e98e
enrico-kaack <e.kaack@live.de>
2020-10-12 10:45:41 +0200

    Fixed: num lstm cells

--- scripts/run_experiments/test_holdout.py
+++ scripts/run_experiments/test_holdout.py
@@ -13,37 +13,37 @@ def run_all_tasks(validation_source, workers):
 
     for problem_type in ProblemType:
         t =[ 
-            #Random Forest
-            TaskTrainRandomForest(problem_type=problem_type, oversampling_enabled=False, undersampling_enabled=False, encode_type=True),
-            TaskTrainRandomForest(problem_type=problem_type, oversampling_enabled=False, undersampling_enabled=False, encode_type=False),
-            TaskTrainRandomForest(problem_type=problem_type, oversampling_enabled=False, undersampling_enabled=False, encode_type=True, class_weight="balanced"),
-            TaskTrainRandomForest(problem_type=problem_type, oversampling_enabled=False, undersampling_enabled=False, encode_type=False, class_weight="balanced"),
+            # #Random Forest
+            # TaskTrainRandomForest(problem_type=problem_type, oversampling_enabled=False, undersampling_enabled=False, encode_type=True),
+            # TaskTrainRandomForest(problem_type=problem_type, oversampling_enabled=False, undersampling_enabled=False, encode_type=False),
+            # TaskTrainRandomForest(problem_type=problem_type, oversampling_enabled=False, undersampling_enabled=False, encode_type=True, class_weight="balanced"),
+            # TaskTrainRandomForest(problem_type=problem_type, oversampling_enabled=False, undersampling_enabled=False, encode_type=False, class_weight="balanced"),
 
-            TaskTrainRandomForest(problem_type=problem_type, oversampling_enabled=True, ratio_after_oversampling=0.5, undersampling_enabled=False, encode_type=False),
-            TaskTrainRandomForest(problem_type=problem_type, oversampling_enabled=True, ratio_after_oversampling=0.5, undersampling_enabled=False, encode_type=True),
-            TaskTrainRandomForest(problem_type=problem_type, oversampling_enabled=True, ratio_after_oversampling=0.5, undersampling_enabled=False, encode_type=True, class_weight="balanced"),
-            TaskTrainRandomForest(problem_type=problem_type, oversampling_enabled=True, ratio_after_oversampling=1.0, undersampling_enabled=False, encode_type=False),
-            TaskTrainRandomForest(problem_type=problem_type, oversampling_enabled=True, ratio_after_oversampling=1.0, undersampling_enabled=False, encode_type=True),
+            # TaskTrainRandomForest(problem_type=problem_type, oversampling_enabled=True, ratio_after_oversampling=0.5, undersampling_enabled=False, encode_type=False),
+            # TaskTrainRandomForest(problem_type=problem_type, oversampling_enabled=True, ratio_after_oversampling=0.5, undersampling_enabled=False, encode_type=True),
+            # TaskTrainRandomForest(problem_type=problem_type, oversampling_enabled=True, ratio_after_oversampling=0.5, undersampling_enabled=False, encode_type=True, class_weight="balanced"),
+            # TaskTrainRandomForest(problem_type=problem_type, oversampling_enabled=True, ratio_after_oversampling=1.0, undersampling_enabled=False, encode_type=False),
+            # TaskTrainRandomForest(problem_type=problem_type, oversampling_enabled=True, ratio_after_oversampling=1.0, undersampling_enabled=False, encode_type=True),
 
-            TaskTrainRandomForest(problem_type=problem_type, oversampling_enabled=False, ratio_after_undersampling=0.5, undersampling_enabled=True, encode_type=False),
-            TaskTrainRandomForest(problem_type=problem_type, oversampling_enabled=False, ratio_after_undersampling=0.5, undersampling_enabled=True, encode_type=True),
-            TaskTrainRandomForest(problem_type=problem_type, oversampling_enabled=False, ratio_after_undersampling=0.5, undersampling_enabled=True, encode_type=True, class_weight="balanced"),
-            TaskTrainRandomForest(problem_type=problem_type, oversampling_enabled=False, ratio_after_undersampling=0.1, undersampling_enabled=True, encode_type=True),
+            # TaskTrainRandomForest(problem_type=problem_type, oversampling_enabled=False, ratio_after_undersampling=0.5, undersampling_enabled=True, encode_type=False),
+            # TaskTrainRandomForest(problem_type=problem_type, oversampling_enabled=False, ratio_after_undersampling=0.5, undersampling_enabled=True, encode_type=True),
+            # TaskTrainRandomForest(problem_type=problem_type, oversampling_enabled=False, ratio_after_undersampling=0.5, undersampling_enabled=True, encode_type=True, class_weight="balanced"),
+            # TaskTrainRandomForest(problem_type=problem_type, oversampling_enabled=False, ratio_after_undersampling=0.1, undersampling_enabled=True, encode_type=True),
 
-            # Gradient Boosting classifier
-            TaskTrainGradientBoostingClassifier(problem_type=problem_type, oversampling_enabled=False, undersampling_enabled=False, learning_rate=0.2, n_estimators=100, subsample=1.0),
-            TaskTrainGradientBoostingClassifier(problem_type=problem_type, oversampling_enabled=False, undersampling_enabled=False, learning_rate=0.2, n_estimators=300, subsample=1.0),
-            TaskTrainGradientBoostingClassifier(problem_type=problem_type, oversampling_enabled=False, undersampling_enabled=False, learning_rate=0.2, n_estimators=200, subsample=1.0),
-            TaskTrainGradientBoostingClassifier(problem_type=problem_type, oversampling_enabled=False, undersampling_enabled=False, learning_rate=0.2, n_estimators=200, subsample=0.4),
-            TaskTrainGradientBoostingClassifier(problem_type=problem_type, oversampling_enabled=False, undersampling_enabled=False, learning_rate=0.2, n_estimators=200, subsample=0.7),
+            # # Gradient Boosting classifier
+            # TaskTrainGradientBoostingClassifier(problem_type=problem_type, oversampling_enabled=False, undersampling_enabled=False, learning_rate=0.2, n_estimators=100, subsample=1.0),
+            # TaskTrainGradientBoostingClassifier(problem_type=problem_type, oversampling_enabled=False, undersampling_enabled=False, learning_rate=0.2, n_estimators=300, subsample=1.0),
+            # TaskTrainGradientBoostingClassifier(problem_type=problem_type, oversampling_enabled=False, undersampling_enabled=False, learning_rate=0.2, n_estimators=200, subsample=1.0),
+            # TaskTrainGradientBoostingClassifier(problem_type=problem_type, oversampling_enabled=False, undersampling_enabled=False, learning_rate=0.2, n_estimators=200, subsample=0.4),
+            # TaskTrainGradientBoostingClassifier(problem_type=problem_type, oversampling_enabled=False, undersampling_enabled=False, learning_rate=0.2, n_estimators=200, subsample=0.7),
             
-            TaskTrainGradientBoostingClassifier(problem_type=problem_type, oversampling_enabled=False, undersampling_enabled=True, ratio_after_undersampling=0.5, learning_rate=0.2, n_estimators=200, subsample=1.0),
-            TaskTrainGradientBoostingClassifier(problem_type=problem_type, oversampling_enabled=False, undersampling_enabled=True, ratio_after_undersampling=0.5, learning_rate=0.2, n_estimators=100, subsample=1.0),
-            TaskTrainGradientBoostingClassifier(problem_type=problem_type, oversampling_enabled=False, undersampling_enabled=True, ratio_after_undersampling=0.5, learning_rate=0.1, n_estimators=100, subsample=1.0),
+            # TaskTrainGradientBoostingClassifier(problem_type=problem_type, oversampling_enabled=False, undersampling_enabled=True, ratio_after_undersampling=0.5, learning_rate=0.2, n_estimators=200, subsample=1.0),
+            # TaskTrainGradientBoostingClassifier(problem_type=problem_type, oversampling_enabled=False, undersampling_enabled=True, ratio_after_undersampling=0.5, learning_rate=0.2, n_estimators=100, subsample=1.0),
+            # TaskTrainGradientBoostingClassifier(problem_type=problem_type, oversampling_enabled=False, undersampling_enabled=True, ratio_after_undersampling=0.5, learning_rate=0.1, n_estimators=100, subsample=1.0),
 
-            TaskTrainGradientBoostingClassifier(problem_type=problem_type, oversampling_enabled=True, undersampling_enabled=False, ratio_after_oversampling=0.5, learning_rate=0.2, n_estimators=200, subsample=1.0),
-            TaskTrainGradientBoostingClassifier(problem_type=problem_type, oversampling_enabled=True, undersampling_enabled=False, ratio_after_oversampling=0.5, learning_rate=0.2, n_estimators=100, subsample=1.0),
-            TaskTrainGradientBoostingClassifier(problem_type=problem_type, oversampling_enabled=True, undersampling_enabled=False, ratio_after_oversampling=0.5, learning_rate=0.1, n_estimators=100, subsample=1.0),
+            # TaskTrainGradientBoostingClassifier(problem_type=problem_type, oversampling_enabled=True, undersampling_enabled=False, ratio_after_oversampling=0.5, learning_rate=0.2, n_estimators=200, subsample=1.0),
+            # TaskTrainGradientBoostingClassifier(problem_type=problem_type, oversampling_enabled=True, undersampling_enabled=False, ratio_after_oversampling=0.5, learning_rate=0.2, n_estimators=100, subsample=1.0),
+            # TaskTrainGradientBoostingClassifier(problem_type=problem_type, oversampling_enabled=True, undersampling_enabled=False, ratio_after_oversampling=0.5, learning_rate=0.1, n_estimators=100, subsample=1.0),
 
         ]
         task_list_ensemble.extend(t)
@@ -58,8 +58,8 @@ def run_all_tasks(validation_source, workers):
             TaskTrainLstm(problem_type=problem_type, oversampling_enabled=False, undersampling_enabled=False, embedding_vecor_length=16, epochs=2, batch_size=256, encode_type=False, num_lstm_cells=10, dropout_emb_lstm=0.2, dropout_lstm_dense=0.2),
             TaskTrainLstm(problem_type=problem_type, oversampling_enabled=False, undersampling_enabled=False, embedding_vecor_length=16, epochs=3, batch_size=256, encode_type=False, num_lstm_cells=10, dropout_emb_lstm=0.2, dropout_lstm_dense=0.2),
 
-            TaskTrainLstm(problem_type=problem_type, oversampling_enabled=False, undersampling_enabled=True, ratio_after_undersampling=0.5, embedding_vecor_length=32, epochs=3, batch_size=256, encode_type=False, dropout_emb_lstm=0.2, dropout_lstm_dense=0.2),
-            TaskTrainLstm(problem_type=problem_type, oversampling_enabled=True, undersampling_enabled=False, ratio_after_oversampling=0.5, embedding_vecor_length=32, epochs=3, batch_size=256, encode_type=False, dropout_emb_lstm=0.2, dropout_lstm_dense=0.2)
+            TaskTrainLstm(problem_type=problem_type, oversampling_enabled=False, undersampling_enabled=True, ratio_after_undersampling=0.5, embedding_vecor_length=32, epochs=3, batch_size=256, encode_type=False, num_lstm_cells=10, dropout_emb_lstm=0.2, dropout_lstm_dense=0.2),
+            TaskTrainLstm(problem_type=problem_type, oversampling_enabled=True, undersampling_enabled=False, ratio_after_oversampling=0.5, embedding_vecor_length=32, epochs=3, batch_size=256, encode_type=False, num_lstm_cells=10, dropout_emb_lstm=0.2, dropout_lstm_dense=0.2)
         ]
         task_list_keras.extend(t)
     
