1c0e42fcd701ff68ec4e2542dac03c06785b7429
enrico-kaack <e.kaack@live.de>
2020-10-13 15:15:24 +0200

    Fixes

--- scripts/analysis/train_sizes.py
+++ scripts/analysis/train_sizes.py
@@ -4,9 +4,9 @@ from collections import Counter
 
 d6tflow.settings.log_level = 'WARNING' # 'DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'
 
-def print_train_size(t):
+def print_train_size(oversampling, undersampling):
     for problem_type in ProblemType:
-        t.problem_type = problem_type
+        t.TaskTrainTestSplit(problem_type=problem_type, oversampling_enabled=oversampling, ratio_after_oversampling=0.5, undersampling_enabled=undersampling, ratio_after_undersampling=0.5,encode_type=True)
         y = t.output()["y_train"].load()
         c = Counter()
         c.update(y)
@@ -15,7 +15,7 @@ def print_train_size(t):
 
         print("\n",problem_type, f"Oversampling: {t.oversampling_enabled} {t.ratio_after_oversampling}|Undersampling: {t.undersampling_enabled} {t.ratio_after_undersampling}")
         print(f"Label {lablel_0_value}: {label_0}")
-        print(f"Label {label_1_value}: {label_1}\n")
+        print(f"Label {label_1_value}: {label_1}")
         print(f"Total length: {len(y)}")
 
 
@@ -26,7 +26,7 @@ print("rm -r data/TaskPrepareXY data/TaskRuleProcessor data/TaskSourceFileToData
 
 print("Final dataset TRAIN")
 
-print_train_size(TaskTrainTestSplit(problem_type=ProblemType.RETURN_NONE, oversampling_enabled=False, ratio_after_oversampling=0.5, undersampling_enabled=False, ratio_after_undersampling=0.5,encode_type=True))
-print_train_size(TaskTrainTestSplit(problem_type=ProblemType.RETURN_NONE, oversampling_enabled=True, ratio_after_oversampling=0.5, undersampling_enabled=False, ratio_after_undersampling=0.5,encode_type=True))
-print_train_size(TaskTrainTestSplit(problem_type=ProblemType.RETURN_NONE, oversampling_enabled=False, ratio_after_oversampling=0.5, undersampling_enabled=True, ratio_after_undersampling=0.5,encode_type=True))
+print_train_size(TaskTrainTestSplit(False, False)
+print_train_size(TaskTrainTestSplit(True, False)
+print_train_size(TaskTrainTestSplit(False, True)
 
