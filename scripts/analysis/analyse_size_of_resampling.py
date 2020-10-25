2d766a804955cc6c65a7fd5602e6ec2df2c6d6e7
enrico-kaack <e.kaack@live.de>
2020-10-25 18:30:30 +0100

    Added: resampling size

--- scripts/analysis/analyse_size_of_resampling.py
+++ scripts/analysis/analyse_size_of_resampling.py
@@ -5,14 +5,13 @@ pd.set_option('display.max_colwidth', None)
 
 from tasks.preprocessing import TaskTrainTestSplit, ProblemType
 
-
-for problem_type in [ProblemType.RETURN_NONE, ProblemType.CONDITION_COMPARISON_SIMPLE, ProblemType.CONDITION_COMPARISON]:
-    with open("size_resampling.txt", "w") as out:
-        data = TaskTrainTestSplit(problem_type=problem_type, oversampling_enabled=True, ratio_after_oversampling = v, undersampling_enabled=False, ratio_after_undersampling=0.5).outputLoad()["y_train"]
-        out.write(f"No resampling: Type: {problem_type}: Size: {len(data)}")
+with open("size_resampling.txt", "w") as out:
+    for problem_type in [ProblemType.RETURN_NONE, ProblemType.CONDITION_COMPARISON_SIMPLE, ProblemType.CONDITION_COMPARISON]:
+        data = TaskTrainTestSplit(problem_type=problem_type, oversampling_enabled=False, ratio_after_oversampling = 0.5, undersampling_enabled=False, ratio_after_undersampling=0.5).output()["y_train"].load()
+        out.write(f"No resampling: Type: {problem_type}: Size: {len(data)}\n")
         for v in [0.5, 1.0]:#oversampling
-            data = TaskTrainTestSplit(problem_type=problem_type, oversampling_enabled=True, ratio_after_oversampling = v, undersampling_enabled=False, ratio_after_undersampling=0.5).outputLoad()["y_train"]
-            out.write(f"Oversampling: {v}: Type: {problem_type}: Size: {len(data)}")
+            data = TaskTrainTestSplit(problem_type=problem_type, oversampling_enabled=True, ratio_after_oversampling = v, undersampling_enabled=False, ratio_after_undersampling=0.5).output()["y_train"].load()
+            out.write(f"Oversampling: {v}: Type: {problem_type}: Size: {len(data)}\n")
         for v in [0.1, 0.5]:#undersampling
-            data = TaskTrainTestSplit(problem_type=problem_type, oversampling_enabled=False, ratio_after_oversampling = 0.5, undersampling_enabled=True, ratio_after_undersampling=v).outputLoad()["y_train"]
-            out.write(f"Undersampling: {v}: Type: {problem_type}: Size: {len(data)}")
+            data = TaskTrainTestSplit(problem_type=problem_type, oversampling_enabled=False, ratio_after_oversampling = 0.5, undersampling_enabled=True, ratio_after_undersampling=v).output()["y_train"].load()
+            out.write(f"Undersampling: {v}: Type: {problem_type}: Size: {len(data)}\n")
