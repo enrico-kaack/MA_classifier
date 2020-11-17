9457ede671af6315b78b59152992b7a64b2c965a
enrico-kaack <e.kaack@live.de>
2020-11-18 00:43:24 +0100

    Changed number of workers to 1

--- run_everything.sh
+++ run_everything.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-python -m scripts.run_experiments.train_all_models final_dataset -n 4
+python -m scripts.run_experiments.train_all_models final_dataset -n 1
 #move generated json files in results in seperate folder
 mkdir results/train_val
 mv results/*.json results/train_val
 
-python -m scripts.run_experiments.test_holdout final_validation -n 4
+python -m scripts.run_experiments.test_holdout final_validation -n 1
 mkdir results/holdout_test
 mv results/*.json results/holdout_test
 
-python -m scripts.run_experiments.test_manipulated_code final_validation -n 4
+python -m scripts.run_experiments.test_manipulated_code final_validation -n 1
 mkdir results/manipulated_code
 mv results/*.json results/manipulated_code
