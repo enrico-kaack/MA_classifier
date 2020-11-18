4ac42ba1a3b2191f6508a0adc68ae1c07e889e0f
enrico-kaack <e.kaack@live.de>
2020-11-18 18:24:42 +0100

    Temp: reduced number of trainings to test the complete runthrough

--- run_everything.sh
+++ run_everything.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-python -m scripts.run_experiments.train_all_models final_dataset -n 1
-#move generated json files in results in seperate folder
 mkdir results/train_val
+python -m scripts.run_experiments.train_all_models final_dataset -n 1
 mv results/*.json results/train_val
 
-python -m scripts.run_experiments.test_holdout final_validation -n 1
 mkdir results/holdout_test
+python -m scripts.run_experiments.test_holdout final_validation -n 1
 mv results/*.json results/holdout_test
 
-python -m scripts.run_experiments.test_manipulated_code final_validation -n 1
 mkdir results/manipulated_code
+python -m scripts.run_experiments.test_manipulated_code final_validation -n 1
 mv results/*.json results/manipulated_code
